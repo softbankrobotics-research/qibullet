90238bf12f687d586697ea27a73c0b1e04afb7d1
Pandhariix <mbusy.pro@gmail.com>
2019-02-10 18:58:03 +0100

    Add additional Python versions for continuous integration testing

--- setup.py
+++ setup.py
@@ -37,7 +37,9 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         "Operating System :: POSIX :: Linux",
