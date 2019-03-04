ab279bea534a25ef8e823ba2ca6dae63f2b4deee
Pandhariix <mbusy.pro@gmail.com>
2019-03-04 10:14:37 +0100

    Update the laser example

--- examples/pepper_lasers.py
+++ examples/pepper_lasers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import random
 import pybullet
 import pybullet_data
 from qibullet import PepperVirtual
@@ -14,21 +13,7 @@ def main():
     pepper = simulation_manager.spawnPepper(client, spawn_ground_plane=True)
 
     pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
-    pybullet.loadURDF(
-        "sphere2red.urdf",
-        basePosition=[0, 2, 0.5],
-        globalScaling=1.0,
-        physicsClientId=client)
-    pybullet.loadURDF(
-        "sphere2red.urdf",
-        basePosition=[2, 0, 1],
-        globalScaling=1.0,
-        physicsClientId=client)
-    pybullet.loadURDF(
-        "duck_vhacd.urdf",
-        basePosition=[-1, 0, 1],
-        globalScaling=5.0,
-        physicsClientId=client)
+
     pybullet.loadURDF(
         "duck_vhacd.urdf",
         basePosition=[1, -1, 0.5],
@@ -39,29 +24,16 @@ def main():
     pepper.subscribeLaser()
     pepper.goToPosture("Stand", 0.6)
 
-    text_id = pybullet.addUserDebugText(
-        "Laser scan",
-        textPosition=[0, 0, 1.4],
-        textColorRGB=[0, 0, 1])
-
     while True:
         laser_list = pepper.getRightLaserValue()
         laser_list.extend(pepper.getFrontLaserValue())
         laser_list.extend(pepper.getLeftLaserValue())
 
-        print laser_list
         if all(laser == 5.6 for laser in laser_list):
-            pybullet.addUserDebugText(
-                "Nothing detected by the lasers",
-                textPosition=[0, 0, 1.4],
-                textColorRGB=[1, 0, 0],
-                replaceItemUniqueId=text_id)
+            print("Nothing detected")
         else:
-            pybullet.addUserDebugText(
-                "Something has been detected by the lasers",
-                textPosition=[0, 0, 1.4],
-                textColorRGB=[0, 1, 0],
-                replaceItemUniqueId=text_id)
+            print("Detected")
+            pass
 
 
 if __name__ == "__main__":
