fa4936f126dbd7997be8970634e4fd03f6fd3091
Maxime Busy <mbusy@aldebaran.com>
2019-04-19 20:00:58 +0200

    Correct path for the images

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    long_description[:long_description.index("<!-- start images -->")] + long_description[(long_description.index("<!-- end images -->")-5):]
 
 
 setuptools.setup(
