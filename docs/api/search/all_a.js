144de458bba0110b90aa23964e404bf323397d61
Pandhariix <mbusy.pro@gmail.com>
2019-06-13 18:03:40 +0200

    Update doc

--- docs/api/search/all_a.js
+++ docs/api/search/all_a.js
@@ -1,13 +1,9 @@
 var searchData=
 [
-  ['setangles',['setAngles',['../classqibullet_1_1pepper__virtual_1_1PepperVirtual.html#a7933a730408c391f04925efa5bb65cf3',1,'qibullet.pepper_virtual.PepperVirtual.setAngles()'],['../classqibullet_1_1robot__virtual_1_1RobotVirtual.html#afd188e15e0ad3e28adc613a139c66a7e',1,'qibullet.robot_virtual.RobotVirtual.setAngles()']]],
-  ['setlinearvelocity',['setLinearVelocity',['../classqibullet_1_1base__controller_1_1BaseController.html#a0498f937cdaec775641df7d212fdaf53',1,'qibullet.base_controller.BaseController.setLinearVelocity()'],['../classqibullet_1_1base__controller_1_1PepperBaseController.html#a390f4cd9f1084259dd6caf960c24d50c',1,'qibullet.base_controller.PepperBaseController.setLinearVelocity()']]],
-  ['setmaxvelocity',['setMaxVelocity',['../classqibullet_1_1joint_1_1Joint.html#a717fa49e92d63cf1a1659840b169406e',1,'qibullet::joint::Joint']]],
-  ['showlaser',['showLaser',['../classqibullet_1_1laser_1_1Laser.html#a8355bbb33dd291090dfcbde13a6baf31',1,'qibullet.laser.Laser.showLaser()'],['../classqibullet_1_1pepper__virtual_1_1PepperVirtual.html#aaa36b354a3c355aa60b9469d306da24b',1,'qibullet.pepper_virtual.PepperVirtual.showLaser()']]],
-  ['simulationmanager',['SimulationManager',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html',1,'qibullet::simulation_manager']]],
-  ['spawnpepper',['spawnPepper',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#aeac971c9a89aab4625031beb947cc088',1,'qibullet::simulation_manager::SimulationManager']]],
-  ['stopsimulation',['stopSimulation',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#a7c3a6687ff2c6e7649e3b080bcce4f3c',1,'qibullet::simulation_manager::SimulationManager']]],
-  ['subscribe',['subscribe',['../classqibullet_1_1camera_1_1Camera.html#aec964b6d84a7be2d6bcd8ab1c11a21a1',1,'qibullet.camera.Camera.subscribe()'],['../classqibullet_1_1camera_1_1CameraRgb.html#aefa98e6de60dcaddda5e1b196d45fd39',1,'qibullet.camera.CameraRgb.subscribe()'],['../classqibullet_1_1camera_1_1CameraDepth.html#a0aab0cf377bc0a5b7291e86a124eb32a',1,'qibullet.camera.CameraDepth.subscribe()'],['../classqibullet_1_1laser_1_1Laser.html#afdb939d93bd545b8480a2bb02d3f4006',1,'qibullet.laser.Laser.subscribe()']]],
-  ['subscribecamera',['subscribeCamera',['../classqibullet_1_1pepper__virtual_1_1PepperVirtual.html#a2778aa3c25fe34e635dbec1b6458cad3',1,'qibullet::pepper_virtual::PepperVirtual']]],
-  ['subscribelaser',['subscribeLaser',['../classqibullet_1_1pepper__virtual_1_1PepperVirtual.html#a51ea08353934c1bcf7966213eb4828d1',1,'qibullet::pepper_virtual::PepperVirtual']]]
+  ['removenao',['removeNao',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#a5141efd57313c9ca1eb362d77f330bc5',1,'qibullet::simulation_manager::SimulationManager']]],
+  ['removepepper',['removePepper',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#af88a89ed9dd37eb1209c4df0ed73aa51',1,'qibullet::simulation_manager::SimulationManager']]],
+  ['resetsimulation',['resetSimulation',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#aff85f8e6738a675779e07d466f9ca159',1,'qibullet::simulation_manager::SimulationManager']]],
+  ['robotmodule',['RobotModule',['../classqibullet_1_1robot__module_1_1RobotModule.html',1,'qibullet::robot_module']]],
+  ['robotposture',['RobotPosture',['../classqibullet_1_1robot__posture_1_1RobotPosture.html',1,'qibullet::robot_posture']]],
+  ['robotvirtual',['RobotVirtual',['../classqibullet_1_1robot__virtual_1_1RobotVirtual.html',1,'qibullet::robot_virtual']]]
 ];
