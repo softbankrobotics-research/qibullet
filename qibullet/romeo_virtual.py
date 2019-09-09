2658e8d2ef528e6e5a5f5c5dd781d272e35351d6
Pandhariix <mbusy.pro@gmail.com>
2019-09-09 18:43:16 +0200

    Add cameras and methods to the RomeoVirtual class. The max efforts needs to be updated to make the robot stand

--- qibullet/romeo_virtual.py
+++ qibullet/romeo_virtual.py
@@ -4,6 +4,7 @@
 import os
 import pybullet
 from qibullet.camera import *
+from qibullet.robot_posture import RomeoPosture
 from qibullet.robot_virtual import RobotVirtual
 
 
@@ -11,19 +12,25 @@ class RomeoVirtual(RobotVirtual):
     """
     Class representing the virtual instance of the Romeo robot
     """
-    ID_CAMERA_TOP = 0
-    ID_CAMERA_BOTTOM = 1
+    ID_CAMERA_RIGHT = 0
+    ID_CAMERA_LEFT = 1
+    ID_CAMERA_DEPTH = 2
     FRAME_WORLD = 1
     FRAME_ROBOT = 2
     URDF_PATH = "robot_data/romeo_H37/romeo.urdf"
+    P_STAND = RomeoPosture("Stand")
+    P_STAND_INIT = RomeoPosture("StandInit")
+    P_STAND_ZERO = RomeoPosture("StandZero")
+    P_CROUCH = RomeoPosture("Crouch")
 
     def __init__(self):
         """
         Constructor
         """
         RobotVirtual.__init__(self, RomeoVirtual.URDF_PATH)
-        self.camera_top = None
-        self.camera_bottom = None
+        self.camera_right = None
+        self.camera_left = None
+        self.camera_depth = None
 
     def loadRobot(self, translation, quaternion, physicsClientId=0):
         """
@@ -68,6 +75,8 @@ class RomeoVirtual(RobotVirtual):
             childFramePosition=translation,
             childFrameOrientation=quaternion,
             physicsClientId=self.physics_client)
+
+        self.goToPosture("Stand", 1.0)
 
         for link in ["torso", "HeadRoll_link"]:
             pybullet.setCollisionFilterPair(
@@ -146,3 +155,262 @@ class RomeoVirtual(RobotVirtual):
             elif 'LFinger' in joint_name or 'LThumb' in joint_name:
                 self.joint_dict[joint_name].setMaxVelocity(
                     self.joint_dict["LHand"].getMaxVelocity())
+
+        self.camera_right = CameraRgb(
+            self.robot_model,
+            self.link_dict["CameraRightEye_optical_frame"],
+            hfov=60.9,
+            vfov=47.6,
+            physicsClientId=self.physics_client)
+
+        self.camera_left = CameraRgb(
+            self.robot_model,
+            self.link_dict["CameraLeftEye_optical_frame"],
+            hfov=60.9,
+            vfov=47.6,
+            physicsClientId=self.physics_client)
+
+        self.camera_depth = CameraRgb(
+            self.robot_model,
+            self.link_dict["CameraDepth_optical_frame"],
+            hfov=58.0,
+            vfov=45.0,
+            physicsClientId=self.physics_client)
+
+    def setAngles(self, joint_names, joint_values, percentage_speed):
+        """
+        Overloads @setAngles from the @RobotVirtual class. Handles the finger
+        mimic behaviour.
+
+        Parameters:
+            joint_names - List of string (or string if only one joint)
+            containing the name of the joints to be controlled
+            joint_values - List of values (or value if only one joint)
+            corresponding to the angles in radians to be applied
+            percentage_speed - Percentage (or percentages) of the max speed to
+            be used for the movement
+        """
+        try:
+            if type(joint_names) is str:
+                assert type(joint_values) is int or type(joint_values) is float
+                names = [joint_names]
+                values = [joint_values]
+            else:
+                assert type(joint_names) is type(joint_values) is list
+                names = list(joint_names)
+                values = list(joint_values)
+
+            if isinstance(percentage_speed, list):
+                speeds = list(percentage_speed)
+            else:
+                speeds = [percentage_speed]*len(names)
+
+        except AssertionError:
+            raise pybullet.error("Error in the parameters given to the\
+                setAngles method")
+
+        for hand in ["RHand", "LHand"]:
+            for i in range(names.count(hand)):
+                index = names.index(hand)
+                value = values[index]
+                speed = speeds[index]
+                names.pop(index)
+                values.pop(index)
+                speeds.pop(index)
+                finger_names, finger_values = self._mimicHand(hand, value)
+                names.extend(finger_names)
+                values.extend(finger_values)
+                speeds.extend([speed]*len(finger_names))
+
+        RobotVirtual.setAngles(
+            self,
+            names,
+            values,
+            speeds)
+
+    def getAnglesPosition(self, joint_names):
+        """
+        Overloads @getAnglesPosition from the @RobotVirtual class. Handles the
+        finger mimicked position for the hands (when getting the position of
+        RHand or LHand, will return the hand's opening percentage).
+
+        Parameters:
+            joint_names - List of string (or string if only one joint)
+            containing the name of the joints
+
+        Returns:
+            joint_positions - List of float (or float if only one joint)
+            containing the joint's positions in radians
+        """
+        if type(joint_names) is str:
+            names = [joint_names]
+        else:
+            names = list(joint_names)
+
+        joint_positions = RobotVirtual.getAnglesPosition(self, names)
+
+        for hand, finger in zip(
+                ["RHand", "LHand"],
+                ["RFinger11", "LFinger11"]):
+            for i in range(names.count(hand)):
+                index = names.index(hand)
+                joint_positions[index] =\
+                    RobotVirtual.getAnglesPosition(self, [finger]).pop() /\
+                    (1/(self.joint_dict[finger].getUpperLimit() -
+                        self.joint_dict[finger].getLowerLimit())) +\
+                    self.joint_dict[finger].getLowerLimit()
+
+        if len(joint_positions) == 1:
+            return joint_positions.pop()
+        else:
+            return joint_positions
+
+    def getAnglesVelocity(self, joint_names):
+        """
+        Overloads @getAnglesVelocity from the @RobotVirtual class. The method
+        won't return the velocity of RHand and LHand joints.
+
+        Parameters:
+            joint_names - List of string (or string if only one joint)
+            containing the name of the joints
+
+        Returns:
+            joint_velocities - List of float (or float if only one joint)
+            containing the joint's velocities in rad/s
+        """
+        if type(joint_names) is str:
+            names = [joint_names]
+        else:
+            names = list(joint_names)
+
+        joint_velocities = RobotVirtual.getAnglesVelocity(self, names)
+
+        if len(joint_velocities) == 1:
+            return joint_velocities.pop()
+        else:
+            return joint_velocities
+
+    def goToPosture(self, posture_name, percentage_speed):
+        """
+        Position the virtual robot into a particular posture. The different
+        available postures are NaoPosture objects.
+
+        Parameters:
+            posture_name - String containing the name of the posture. The
+            posture name is not case-sensitive
+            percentage_speed - Percentage of the max speed to be used for the
+            movement
+
+        Returns:
+            Boolean - True if the posture can be applied, False otherwise
+        """
+        posture_list = [
+            RomeoVirtual.P_STAND,
+            RomeoVirtual.P_STAND_INIT,
+            RomeoVirtual.P_STAND_ZERO,
+            RomeoVirtual.P_CROUCH]
+
+        for posture in posture_list:
+            if posture.isPostureName(posture_name):
+                self.setAngles(
+                    posture.getPostureJointNames(),
+                    posture.getPostureJointValues(),
+                    percentage_speed)
+
+                return True
+
+        return False
+
+    def subscribeCamera(self, camera_id, resolution=Camera.K_QVGA):
+        """
+        Subscribe to the camera holding the camera id. WARNING: at the moment,
+        only one camera can be subscribed.
+
+        Parameters:
+            camera_id - The id of the camera to be subscribed
+            resolution - CameraResolution object, the resolution of the camera
+        """
+        if camera_id == RomeoVirtual.ID_CAMERA_RIGHT:
+            self.camera_right.subscribe(resolution=resolution)
+        elif camera_id == RomeoVirtual.ID_CAMERA_LEFT:
+            self.camera_left.subscribe(resolution=resolution)
+        elif camera_id == RomeoVirtual.ID_CAMERA_DEPTH:
+            self.camera_depth.subscribe(resolution=resolution)
+
+    def unsubscribeCamera(self, camera_id):
+        """
+        Unsubscribe from a camera, the one holding the camera id.
+
+        Parameters:
+            camera_id - The id of the camera to be unsubscribed
+        """
+        if camera_id == RomeoVirtual.ID_CAMERA_RIGHT:
+            self.camera_right.unsubscribe()
+        elif camera_id == RomeoVirtual.ID_CAMERA_LEFT:
+            self.camera_left.unsubscribe()
+        elif camera_id == RomeoVirtual.ID_CAMERA_DEPTH:
+            self.camera_depth.unsubscribe()
+
+    def getCameraFrame(self):
+        """
+        Returns a camera frame. Be advised that the subscribeCamera method
+        needs to be called beforehand.
+
+        Returns:
+            frame - The current camera frame as a formatted numpy array,
+            directly exploitable from OpenCV
+        """
+        if self.camera_right.isActive():
+            return self.camera_right.getFrame()
+        elif self.camera_left.isActive():
+            return self.camera_left.getFrame()
+        elif self.camera_depth.isActive():
+            return self.camera_depth.getFrame()
+
+    def getCameraResolution(self):
+        """
+        Returns the resolution of the active camera. If no camera is active,
+        returns None
+
+        Returns:
+            resolution - a CameraResolution object describing the resolution of
+            the active camera
+        """
+        if self.camera_right.isActive():
+            return self.camera_right.getResolution()
+        elif self.camera_left.isActive():
+            return self.camera_left.getResolution()
+        elif self.camera_depth.isActive():
+            return self.camera_depth.getResolution()
+
+    def _mimicHand(
+            self,
+            hand,
+            value,
+            multiplier=0.988205,
+            offset=0.0):
+        """
+        Used to propagate a joint value on the fingers attached to the hand.
+        The formula used to mimic a joint is the following (with a multiplier
+        of 0.999899 and an offset of 0.0):
+
+        finger_value = (hand_value * multiplier) + offset
+
+        Parameters:
+            hand - String, RHand or LHand
+            value - the joint value to be propagated
+
+        Returns:
+            finger_names - Names of the finger to be controlled
+            finger_values - Values of the fingers to be controlled
+        """
+        finger_names = list()
+        finger_values = list()
+
+        for joint_name in self.joint_dict.keys():
+            if (hand[0] + "Finger") in joint_name or\
+               (hand[0] + "Thumb") in joint_name:
+                finger_names.append(joint_name)
+                finger_values.append((value * multiplier) + offset)
+
+        return finger_names, finger_values
