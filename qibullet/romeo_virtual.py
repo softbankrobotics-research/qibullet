84bc74bf0bdc9d6aa05f95e43d2ca829e1cdebc4
Pandhariix <mbusy.pro@gmail.com>
2019-09-09 17:42:15 +0200

    Add self collision filters for the RomeoVirtual class

--- qibullet/romeo_virtual.py
+++ qibullet/romeo_virtual.py
@@ -48,10 +48,101 @@ class RomeoVirtual(RobotVirtual):
             physicsClientId=physicsClientId)
 
         # Add 0.50 meters on the z component, avoing to spawn NAO in the ground
-        translation = [translation[0], translation[1], translation[2] + 0.50]
+        translation = [translation[0], translation[1], translation[2] + 1.0]
 
         RobotVirtual.loadRobot(
             self,
             translation,
             quaternion,
             physicsClientId=physicsClientId)
+
+        balance_constraint = pybullet.createConstraint(
+            parentBodyUniqueId=self.robot_model,
+            parentLinkIndex=-1,
+            childBodyUniqueId=-1,
+            childLinkIndex=-1,
+            jointType=pybullet.JOINT_FIXED,
+            jointAxis=[0, 0, 0],
+            parentFramePosition=[0, 0, 0],
+            parentFrameOrientation=[0, 0, 0, 1],
+            childFramePosition=translation,
+            childFrameOrientation=quaternion,
+            physicsClientId=self.physics_client)
+
+        for link in ["torso", "HeadRoll_link"]:
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict["NeckPitch_link"].getIndex(),
+                self.link_dict[link].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+
+        for side in ["R", "L"]:
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict[side + "Eye"].getIndex(),
+                self.link_dict["HeadRoll_link"].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict[side + "Thigh"].getIndex(),
+                self.link_dict["body"].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict[side.lower() + "_ankle"].getIndex(),
+                self.link_dict[side + "Tibia"].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict[side + "ShoulderYaw_link"].getIndex(),
+                self.link_dict["torso"].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+            pybullet.setCollisionFilterPair(
+                self.robot_model,
+                self.robot_model,
+                self.link_dict[side + "WristRoll_link"].getIndex(),
+                self.link_dict[side.lower() + "_wrist"].getIndex(),
+                0,
+                physicsClientId=self.physics_client)
+
+            for link in ["ShoulderYaw_link", "WristYaw_link"]:
+                pybullet.setCollisionFilterPair(
+                    self.robot_model,
+                    self.robot_model,
+                    self.link_dict[side + link].getIndex(),
+                    self.link_dict[side + "Elbow"].getIndex(),
+                    0,
+                    physicsClientId=self.physics_client)
+
+            for name, link in self.link_dict.items():
+                if side + "finger" in name.lower() or\
+                   side + "thumb" in name.lower():
+                    pybullet.setCollisionFilterPair(
+                        self.robot_model,
+                        self.robot_model,
+                        self.link_dict[side.lower + "_wrist"].getIndex(),
+                        link.getIndex(),
+                        0,
+                        physicsClientId=self.physics_client)
+
+        pybullet.removeConstraint(
+            balance_constraint,
+            physicsClientId=self.physics_client)
+
+        for joint_name in list(self.joint_dict):
+            if 'RFinger' in joint_name or 'RThumb' in joint_name:
+                self.joint_dict[joint_name].setMaxVelocity(
+                    self.joint_dict["RHand"].getMaxVelocity())
+            elif 'LFinger' in joint_name or 'LThumb' in joint_name:
+                self.joint_dict[joint_name].setMaxVelocity(
+                    self.joint_dict["LHand"].getMaxVelocity())
