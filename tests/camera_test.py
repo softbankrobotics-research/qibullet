a49d9cfb015b7b41fb35134577c7157bf5ac235f
Pandhariix <mbusy.pro@gmail.com>
2019-09-19 16:54:46 +0200

    Better camera unit tests, add unit tests for Romeo's cameras

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -4,7 +4,7 @@ import sys
 import unittest
 import pybullet
 from qibullet import SimulationManager
-from qibullet import NaoVirtual, PepperVirtual
+from qibullet import NaoVirtual, PepperVirtual, RomeoVirtual
 from qibullet import Camera, CameraRgb, CameraDepth, CameraResolution
 
 
@@ -19,39 +19,15 @@ class CameraTest(unittest.TestCase):
         """
         physics_client = CameraTest.client
 
-        CameraTest.robot.subscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_TOP)
-        self.assertEqual(
-            Camera.ACTIVE_CAMERA_ID[physics_client],
-            id(CameraTest.robot.camera_top))
-
-        CameraTest.robot.unsubscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_TOP)
-        self.assertEqual(
-            Camera.ACTIVE_CAMERA_ID[physics_client],
-            -1)
-
-        CameraTest.robot.subscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_BOTTOM)
-        self.assertEqual(
-            Camera.ACTIVE_CAMERA_ID[physics_client],
-            id(CameraTest.robot.camera_bottom))
-
-        CameraTest.robot.unsubscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_BOTTOM)
-        self.assertEqual(
-            Camera.ACTIVE_CAMERA_ID[physics_client],
-            -1)
-
-        if isinstance(CameraTest.robot, PepperVirtual):
+        for camera_id, camera_obj in CameraTest.cameras.items():
             CameraTest.robot.subscribeCamera(
-                CameraTest.robot.__class__.ID_CAMERA_DEPTH)
+                camera_id)
             self.assertEqual(
                 Camera.ACTIVE_CAMERA_ID[physics_client],
-                id(CameraTest.robot.camera_depth))
+                id(camera_obj))
 
             CameraTest.robot.unsubscribeCamera(
-                CameraTest.robot.__class__.ID_CAMERA_DEPTH)
+                camera_id)
             self.assertEqual(
                 Camera.ACTIVE_CAMERA_ID[physics_client],
                 -1)
@@ -61,40 +37,40 @@ class CameraTest(unittest.TestCase):
         Test the resolutions for the cameras
         """
         for resolution in [Camera.K_VGA, Camera.K_QVGA, Camera.K_QQVGA]:
-            CameraTest.robot.subscribeCamera(
-                CameraTest.robot.__class__.ID_CAMERA_TOP,
-                resolution=resolution)
+            for camera_id in CameraTest.cameras.keys():
+                CameraTest.robot.subscribeCamera(
+                    camera_id,
+                    resolution=resolution)
 
-            self.assertEqual(
-                CameraTest.robot.getCameraFrame().shape[1],
-                resolution.width)
-            self.assertEqual(
-                CameraTest.robot.getCameraFrame().shape[0],
-                resolution.height)
+                self.assertEqual(
+                    CameraTest.robot.getCameraFrame().shape[1],
+                    resolution.width)
+                self.assertEqual(
+                    CameraTest.robot.getCameraFrame().shape[0],
+                    resolution.height)
 
     def test_camera_channels(self):
         """
         Test the number of channels for each camera.
         """
+        for camera_id in CameraTest.cameras.keys():
+            # If the camera is a depth camera
+            if camera_id == PepperVirtual.ID_CAMERA_DEPTH or\
+                    camera_id == RomeoVirtual.ID_CAMERA_DEPTH:
 
-        CameraTest.robot.subscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_TOP)
-        self.assertEqual(
-            CameraTest.robot.getCameraFrame().shape[2],
-            3)
+                CameraTest.robot.subscribeCamera(
+                    camera_id)
+                self.assertEqual(
+                    len(CameraTest.robot.getCameraFrame().shape),
+                    2)
 
-        CameraTest.robot.subscribeCamera(
-            CameraTest.robot.__class__.ID_CAMERA_BOTTOM)
-        self.assertEqual(
-            CameraTest.robot.getCameraFrame().shape[2],
-            3)
-
-        if isinstance(CameraTest.robot, PepperVirtual):
-            CameraTest.robot.subscribeCamera(
-                CameraTest.robot.__class__.ID_CAMERA_DEPTH)
-            self.assertEqual(
-                len(CameraTest.robot.getCameraFrame().shape),
-                2)
+            # if the camera is an RGB camera
+            else:
+                CameraTest.robot.subscribeCamera(
+                    camera_id)
+                self.assertEqual(
+                    CameraTest.robot.getCameraFrame().shape[2],
+                    3)
 
 
 class PepperCameraTest(CameraTest):
@@ -114,6 +90,11 @@ class PepperCameraTest(CameraTest):
         CameraTest.robot = CameraTest.simulation.spawnPepper(
             CameraTest.client,
             spawn_ground_plane=True)
+
+        CameraTest.cameras = {
+            PepperVirtual.ID_CAMERA_TOP: CameraTest.robot.camera_top,
+            PepperVirtual.ID_CAMERA_BOTTOM: CameraTest.robot.camera_bottom,
+            PepperVirtual.ID_CAMERA_DEPTH: CameraTest.robot.camera_depth}
 
     @classmethod
     def tearDownClass(cls):
@@ -150,6 +131,51 @@ class NaoCameraTest(CameraTest):
         CameraTest.robot = CameraTest.simulation.spawnNao(
             CameraTest.client,
             spawn_ground_plane=True)
+
+        CameraTest.cameras = {
+            NaoVirtual.ID_CAMERA_TOP: CameraTest.robot.camera_top,
+            NaoVirtual.ID_CAMERA_BOTTOM: CameraTest.robot.camera_bottom}
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Stops the simulation
+        """
+        CameraTest.simulation.stopSimulation(
+            CameraTest.client)
+
+    def test_camera_subscribe(self):
+        CameraTest.test_camera_subscribe(self)
+
+    def test_camera_resolutions(self):
+        CameraTest.test_camera_resolutions(self)
+
+    def test_camera_channels(self):
+        CameraTest.test_camera_channels(self)
+
+
+class RomeoCameraTest(CameraTest):
+    """
+    Unittests for Romeo virtual cameras
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Launches a simulation and spawns the Romeo virtual robot
+        """
+        CameraTest.simulation = SimulationManager()
+        CameraTest.client = CameraTest.simulation.launchSimulation(
+            gui=False)
+
+        CameraTest.robot = CameraTest.simulation.spawnRomeo(
+            CameraTest.client,
+            spawn_ground_plane=True)
+
+        CameraTest.cameras = {
+            RomeoVirtual.ID_CAMERA_LEFT: CameraTest.robot.camera_left,
+            RomeoVirtual.ID_CAMERA_RIGHT: CameraTest.robot.camera_right,
+            RomeoVirtual.ID_CAMERA_DEPTH: CameraTest.robot.camera_depth}
 
     @classmethod
     def tearDownClass(cls):
