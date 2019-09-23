8a02350468aae0bd80158e3bc2328abfbbf2b83f
Pandhariix <mbusy.pro@gmail.com>
2019-09-23 12:56:18 +0200

    Update version tag from 1.1.4.1 to 1.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qibullet",
-    version="1.1.4.1",
+    version="1.2.0",
     author="Maxime Busy, Maxime Caniot",
     author_email="",
     description="Bullet-based simulation for SoftBank Robotics' robots",
