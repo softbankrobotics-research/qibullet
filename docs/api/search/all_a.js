575826c4856983e45986eed0f75b8b8d67656b02
Pandhariix <mbusy.pro@gmail.com>
2019-09-23 14:39:02 +0200

    Update documentation

--- docs/api/search/all_a.js
+++ docs/api/search/all_a.js
@@ -2,8 +2,11 @@ var searchData=
 [
   ['removenao',['removeNao',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#a5141efd57313c9ca1eb362d77f330bc5',1,'qibullet::simulation_manager::SimulationManager']]],
   ['removepepper',['removePepper',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#af88a89ed9dd37eb1209c4df0ed73aa51',1,'qibullet::simulation_manager::SimulationManager']]],
+  ['removeromeo',['removeRomeo',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#a361dc6d92f2cd55ee94bb03021ec37c7',1,'qibullet::simulation_manager::SimulationManager']]],
   ['resetsimulation',['resetSimulation',['../classqibullet_1_1simulation__manager_1_1SimulationManager.html#aff85f8e6738a675779e07d466f9ca159',1,'qibullet::simulation_manager::SimulationManager']]],
   ['robotmodule',['RobotModule',['../classqibullet_1_1robot__module_1_1RobotModule.html',1,'qibullet::robot_module']]],
   ['robotposture',['RobotPosture',['../classqibullet_1_1robot__posture_1_1RobotPosture.html',1,'qibullet::robot_posture']]],
-  ['robotvirtual',['RobotVirtual',['../classqibullet_1_1robot__virtual_1_1RobotVirtual.html',1,'qibullet::robot_virtual']]]
+  ['robotvirtual',['RobotVirtual',['../classqibullet_1_1robot__virtual_1_1RobotVirtual.html',1,'qibullet::robot_virtual']]],
+  ['romeoposture',['RomeoPosture',['../classqibullet_1_1robot__posture_1_1RomeoPosture.html',1,'qibullet::robot_posture']]],
+  ['romeovirtual',['RomeoVirtual',['../classqibullet_1_1romeo__virtual_1_1RomeoVirtual.html',1,'qibullet::romeo_virtual']]]
 ];
