c8c1e801bfcbff9ae148c36d4ad971d4990cde18
Maxime Busy <mbusy@aldebaran.com>
2019-12-24 17:37:56 +0100

    Update documentation

--- docs/api/search/classes_6.js
+++ docs/api/search/classes_6.js
@@ -4,5 +4,7 @@ var searchData=
   ['robotposture',['RobotPosture',['../classqibullet_1_1robot__posture_1_1RobotPosture.html',1,'qibullet::robot_posture']]],
   ['robotvirtual',['RobotVirtual',['../classqibullet_1_1robot__virtual_1_1RobotVirtual.html',1,'qibullet::robot_virtual']]],
   ['romeoposture',['RomeoPosture',['../classqibullet_1_1robot__posture_1_1RomeoPosture.html',1,'qibullet::robot_posture']]],
-  ['romeovirtual',['RomeoVirtual',['../classqibullet_1_1romeo__virtual_1_1RomeoVirtual.html',1,'qibullet::romeo_virtual']]]
+  ['romeoroswrapper',['RomeoRosWrapper',['../classqibullet_1_1ros__wrapper_1_1RomeoRosWrapper.html',1,'qibullet::ros_wrapper']]],
+  ['romeovirtual',['RomeoVirtual',['../classqibullet_1_1romeo__virtual_1_1RomeoVirtual.html',1,'qibullet::romeo_virtual']]],
+  ['roswrapper',['RosWrapper',['../classqibullet_1_1ros__wrapper_1_1RosWrapper.html',1,'qibullet::ros_wrapper']]]
 ];
