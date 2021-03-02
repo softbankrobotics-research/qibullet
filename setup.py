20f04b38e2085fc17fcf7e7a40f4d01647f8d5d6
Maxime Busy <mbusy.pro@gmail.com>
2021-03-02 18:02:57 +0100

    Specify Python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -119,6 +119,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         "Operating System :: POSIX :: Linux",
